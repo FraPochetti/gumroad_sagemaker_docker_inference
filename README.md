1962f86b66b5e88eb235d4749d73e5dd7bd2e796
Ubuntu <ubuntu@ip-172-31-17-151.eu-west-1.compute.internal>
2023-09-25 17:47:56 +0000

    pushing files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# gumroad_sagemaker_docker_inference
+# Gumroad Sagemaker Docker Inference Course
 Code coming with the Gumroad course on how to deploy a ML model to Amazon SageMaker with a custom Docker container
+
+Link to Gumroad page
---
+++ gumroad_sagemaker/sagemaker_deploy/build_and_push_to_ecr.sh
@@ -0,0 +1,17 @@
+TAG="gumroad"
+IMAGE_URI="custom-images"
+ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+REGION="eu-west-1"
+
+docker build -t $TAG -f Dockerfile.ModelLocal .
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
+docker tag $TAG:latest $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$IMAGE_URI:$TAG
+docker push $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$IMAGE_URI:$TAG
+
+aws ecr describe-repositories --region $REGION \
+| jq --raw-output .repositories[].repositoryName \
+| while read repo; do  
+    imageIds=$(aws ecr list-images --region $REGION --repository-name $repo --filter tagStatus=UNTAGGED --query 'imageIds[*]' --output json | jq -r '[.[].imageDigest] | map("imageDigest="+.) | join (" ")');
+    if [[ "$imageIds" == "" ]]; then continue; fi
+    aws ecr batch-delete-image --region $REGION --repository-name $repo --image-ids $imageIds; 
+done
